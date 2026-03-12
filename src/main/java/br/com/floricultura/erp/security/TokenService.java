a31dd4f4fe8ef4e18fcfaa6a4435c1cd2c6b175d
Your Name <you@example.com>
2026-03-12 20:23:48 -0300

    🔐 feat: implementa TokenService para geração e validação de JWT

---
+++ src/main/java/br/com/floricultura/erp/security/TokenService.java
@@ -0,0 +1,39 @@
+package br.com.floricultura.erp.security;
+import br.com.floricultura.erp.model.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    public String generateToken(Usuario usuario) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.create()
+                .withIssuer("floricultura-api")
+                .withSubject(usuario.getEmail())
+                .withExpiresAt(generateExpirationDate())
+                .sign(algorithm);
+    }
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("floricultura-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+}

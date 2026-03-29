551b1b13cd1cd0042f4e2564b3f2aadcb214fa3a
Murilex <murilo.bertalha@gmail.com>
2026-03-29 10:31:00 -0300

    ✨ feat(core): habilita processamento assíncrono e agendamento de tarefa

--- src/main/java/br/com/floricultura/erp/ErpApplication.java
+++ src/main/java/br/com/floricultura/erp/ErpApplication.java
@@ -1,13 +1,15 @@
 package br.com.floricultura.erp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
+
 public class ErpApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ErpApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ErpApplication.class, args);
+    }
 }
